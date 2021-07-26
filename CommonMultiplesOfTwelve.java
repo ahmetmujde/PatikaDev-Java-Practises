0398209391a4b8c2c737ed59848adcaf28cfac56
ahmetmujde <mujde.ahmet79@gmail.com>
2021-07-26 15:02:47 +0300

    Common multiples of twelve

---
+++ CommonMultiplesOfTwelve.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CommonMultiplesOfTwelve {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+
+        System.out.println("Please input the any number");
+        number=scanner.nextInt();
+
+        System.out.println("Common multiples of twelve");
+        for (int i=0; i<=number; i++){
+            if ((i%3==0) && (i%4==0))
+                System.out.print(i+", ");
+        }
+
+    }
+}
