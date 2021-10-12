a27e7fb7a0aca4df01a75fb52a4b434649d882d4
ahmetmujde <mujde.ahmet79@gmail.com>
2021-10-12 14:02:18 +0300

    [AVERAGE-OF-HARMONIC-SERIES-SUM]
    Following features are developed.
    
    SalaryCalculator created ->
    - AverageOfHarmonicSeriesSum.java page is developed.
    
    Topic Url : https://app.patika.dev/moduller/java101/pratik-array-ortalama

---
+++ AverageOfHarmonicSeriesSum.java
@@ -0,0 +1,15 @@
+public class AverageOfHarmonicSeriesSum {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        double sum = 0;
+        int n = numbers.length;
+
+        for (int i = 0; i < n; i++) {
+            sum += 1 / (double) numbers[i];
+        }
+
+        System.out.println(n / sum);
+    }
+}
