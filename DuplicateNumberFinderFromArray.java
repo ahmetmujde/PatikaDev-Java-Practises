dfe1aabd63c9fdc47833a812e90b2f5d8f7195c8
ahmetmujde <mujde.ahmet79@gmail.com>
2021-10-14 19:26:01 +0300

    [DUPLICATE-NUMBER-FINDER-FROM-ARRAY]
    Following features are developed.
    - DuplicateNumberFinderFromArray.
    
    Topic Url : https://app.patika.dev/moduller/java101/pratik-duplicate

--- DuplicateNumberFinderFromArray.java
+++ DuplicateNumberFinderFromArray.java
@@ -12,9 +12,10 @@ public class DuplicateNumberFinderFromArray {
     static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
-                System.out.println(arr[i]);
+                if (arr[i] % 2 == 0) {
+                    System.out.println(arr[i]);
+                }
             }
-
         }
     }
 
